4c28da70d60c2e036eba4ca0ecdc01561ab56679
Teo <teobox11@gmail.com>
2021-04-30 17:47:59 +0300

    installed jest and eslint

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ const button = document.querySelector('button');
 const div = document.querySelector('.counter');
 
 let counter = 0;
+
 const fn = () => {
   counter -= 1;
   return counter;
 };
 
-button.addEventListener('click', (e) => {
+button.addEventListener('click', () => {
   const num = fn();
   div.textContent = num;
 });
-
