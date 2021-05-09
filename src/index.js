5945fdc11a2419759de42e3ce202aff75ac3e22a
Teo <teobox11@gmail.com>
2021-05-09 15:17:23 +0300

    made start page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const button = document.querySelector('button');
 const div = document.querySelector('.counter');
--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,11 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
-      }
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
