823e66e08e80f46db8ff9ab90475812028272096
Teo <teobox11@gmail.com>
2021-05-13 23:55:10 +0300

    deleted test in index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-const button = document.querySelector('button');
-const div = document.querySelector('.counter');
 
-let counter = 0;
-
-const fn = () => {
-  counter -= 1;
-  return counter;
-};
-
-button.addEventListener('click', () => {
-  const num = fn();
-  div.textContent = num;
-});
