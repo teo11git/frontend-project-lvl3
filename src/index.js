0035a3ab35f770872b68a32e95bfafd31ae92f20
Teo <teobox11@gmail.com>
2021-05-15 19:42:40 +0300

    wrote getList

--- src/index.js
+++ src/index.js
@@ -2,13 +2,40 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import axios from 'axios';
 
-const meduzaUrl = 'https://habr.com/ru/rss/all/all/?fl=ru';
+const meduzaUrl = 'https://meduza.io/rss2/all';
 
 const useProxy = (url) => `https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(url)}`;
 
+const parse = (data) => {
+  const XMLdocument = new DOMParser().parseFromString(data, 'application/xml');
+  console.log(XMLdocument);
+  return XMLdocument;
+};
+
+const getList = (xml) => {
+  const items = xml.querySelectorAll('item');
+  const list = [ ...items ].map((item) => {
+    const post = {
+      title: item.querySelector('title').textContent,
+      description: item.querySelector('description').textContent,
+      link: item.querySelector('link').textContent,
+    };
+    return post;
+  });
+  console.log(list);
+}
+
 axios.get(useProxy(meduzaUrl))
-  .then((res) => res.data.content)
+  .then((res) => {
+    // console.log(res.data.contents);
+    return res.data.contents;
+  })
   .then((str) => {
-    console.log(str);
-    return new DOMParser().parseFromString(str, 'application/xml')})
-  .then(console.log);
+    // console.log(str);
+    // parse(str);
+    return parse(str)
+  })
+  .then((xml) => {
+    const items = getList(xml);
+    console.log(items);
+  });
