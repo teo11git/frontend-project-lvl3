dea878b7bfb0ceda6cb8bd820dd6ea618d1790b2
Teo <teobox11@gmail.com>
2021-05-15 18:14:26 +0300

    expirience with axios

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import axios from 'axios';
 
+const meduzaUrl = 'https://habr.com/ru/rss/all/all/?fl=ru';
 
+const useProxy = (url) => `https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(url)}`;
+
+axios.get(useProxy(meduzaUrl))
+  .then((res) => res.data.content)
+  .then((str) => {
+    console.log(str);
+    return new DOMParser().parseFromString(str, 'application/xml')})
+  .then(console.log);
