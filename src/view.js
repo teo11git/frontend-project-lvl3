03f66a4c78c2688dbb8ad6744b8f19d75446c117
Teo <teobox11@gmail.com>
2021-05-21 18:57:05 +0300

    edit view. added cases

--- src/view.js
+++ src/view.js
@@ -1,7 +1,20 @@
 import onChange from 'on-change';
 
 const render = (state, elements) => (path, value) => {
-  
+  switch(value) {
+    case 'filling':
+      console.log('process setted as filling');
+      break;
+    case 'sending':
+      console.log('process setted as sending');
+      break;
+    case 'access fault':
+      console.log('process setted as accesss fault');
+      break;
+    case 'validation fault':
+      console.log('process setted as validation fault');
+      break;
+  } 
 };
 
 export default render;
